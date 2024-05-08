e0dfb330904bbf4c428038745001c04ad49e10bc
NguyenTan <108332505+nguyentanjr@users.noreply.github.com>
2024-05-08 19:33:48 +0700

    Update Phan3.cpp

--- Week 3/Phan3.cpp
+++ Week 3/Phan3.cpp
@@ -1,63 +1,46 @@
 #include<bits/stdc++.h>
-
 using namespace std;
-
-
 #include<bits/stdc++.h>
-
 using namespace std;
-
 struct Point
 {
     int x, y;
-
     Point midPoint(Point a1, Point a2) const
     {
-        Point res;
-        res.x = (a1.x + a2.x)/2;
-        res.y = (a1.y + a2.y)/2;
-        return res;
+        Point a;
+        a.x = (a1.x + a2.x)/2;
+        a.y = (a1.y + a2.y)/2;
+        return a;
     }
 };
-
 struct Rect
 {
     float x,y,w,h;
-
     bool contain(Point a)
     {
-        if(a.x >= x && a.x<=a.x+w && a.y<= y && y <= a.y+h)
+        if(a.x >= x && a.x <= a.x + w && a.y <= y && y <= a.y + h)
         {
             return true;
         }
         return true;
     }
 };
-
 struct Ship
 {
     Rect ship;
     string id;
     float dx,dy;
     void move(Rect &ship){
-        ship.x+=dx;
-        ship.y+=dy;
+        ship.x += dx;
+        ship.y += dy;
     }
 };
-
 void display(const Ship& ship) {
-    cout << ship.id << endl;
-    cout << ship.ship.x << endl;
-    cout << ship.ship.y << endl;
+    cout << ship.id << " " << ship.ship.x << " " << ship.ship.y;
 }
-
-
 int main() {
     Point p;
-    p.x = 2;
-    p.y = 3;
-
+    p.x = 4;
+    p.y = 5;
     cout << &p << " " << &p.x << " " << &p.y << endl;
-
-    // &p == &p.
 }
