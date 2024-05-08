9342f229a7aa0d5f473779040516c98f7fb5da9c
NguyenTan <108332505+nguyentanjr@users.noreply.github.com>
2024-05-08 19:56:40 +0700

    Update 03.cpp

--- Week 4/Part 1/03.cpp
+++ Week 4/Part 1/03.cpp
@@ -1,16 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 bool solve(string s){
-    string t=s;
+    string t = s;
 	reverse(s.begin(), s.end());
-	if(s==t) return true;
+	if(s == t) return true;
     else return false;
 }
 int main(){
-	int n;
+    int n;
     cin >> n;
     while(n--){
-        int a,b,res = 0;
+        int a, b, res = 0;
         cin >> a >> b;
         for(int i = a; i <= b; i++){
             string s;
