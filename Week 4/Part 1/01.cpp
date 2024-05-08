fb2429bf3cb1d86bbd87006b74facede607ef28b
NguyenTan <108332505+nguyentanjr@users.noreply.github.com>
2024-05-08 19:54:40 +0700

    Update 01.cpp

--- Week 4/Part 1/01.cpp
+++ Week 4/Part 1/01.cpp
@@ -1,18 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
-int cnt[1000];
-int main(){
-	int n; cin >>n;
-	int a[n];
+int cnt[10005];
+int main()
+{
+	int n; 
+	cin >>n;
+	int a[10005];
 	for(int i = 0; i < n; i++) cin >>a[i];
 	for(int i = 0; i < n; i++){
 		cnt[a[i]]++;
-}
-    for(int i = 0; i < n; i++){
-    		if(cnt[a[i]] > 1){
+	}
+	for(int i = 0; i < n; i++){
+		if(cnt[a[i]] > 1)
+		{
 			cout <<"YES";
 			return 0;
 		}
 	}
 	cout <<"NO";
-	}
+}
